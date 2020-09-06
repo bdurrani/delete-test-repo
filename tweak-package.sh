39c2cfe82608b10efa0a5a058f61a4d4dcc325f5
Bilal Durrani <bilal33@gmail.com>
2020-09-06 15:31:25 -0400

    prerelease

--- tweak-package.sh
+++ tweak-package.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # printf '%s\n\n' "$current_time" >> README.md
-npm version patch
+npm version prerelease --preid=alpha
 npm i --package-lock-only
 git pull origin 
 git push origin develop --tags 
