3259bc18c76fd633eda8ca26ce640b0ce118568b
Bilal Durrani <bilal33@gmail.com>
2020-09-06 09:15:30 -0400

    change: 2020.09.06-09.15.23

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-printf '%s\n\n' "$current_time" >> README.md
+# printf '%s\n\n' "$current_time" >> README.md
+npm version minor
+npm ci
 
 git add .
 git commit -m "change: ${current_time}"
 
 echo "Local change made. Wait for remote change"
-read -t 3 -n 1
+read year
+# read -t 3 -n 1
 
 echo "Pulling"
 git pull origin
